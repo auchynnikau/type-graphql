f7140d4f4238fbb4de2a89e03fa97b6b7520e00e
Viachaslau Auchynnikau <33329898+auchynnikau@users.noreply.github.com>
2021-12-20 16:36:52 +0300

    feat: register validation (#3)

--- src/modules/user/register.ts
+++ src/modules/user/register.ts
@@ -1,32 +1,18 @@
-import {
-  Query,
-  Mutation,
-  Resolver,
-  Arg,
-  FieldResolver,
-  Root,
-} from "type-graphql";
-import * as bcrypt from "bcryptjs";
+import { Query, Mutation, Resolver, Arg } from "type-graphql";
+import bcrypt from "bcryptjs";
 import { User } from "../../entities/user";
+import { RegisterInput } from "./register/registerInput";
 
-@Resolver(User)
+@Resolver()
 export class RegisterResolver {
   @Query(() => String)
   async hello() {
     return "Hello world!";
   }
 
-  @FieldResolver()
-  async name(@Root() parent: User) {
-    return `${parent.firstName} ${parent.lastName}`;
-  }
-
   @Mutation(() => User)
   async register(
-    @Arg("firstName") firstName: string,
-    @Arg("lastName") lastName: string,
-    @Arg("email") email: string,
-    @Arg("password") password: string
+    @Arg("data") { email, firstName, lastName, password }: RegisterInput
   ): Promise<User> {
     const hashedPassword = await bcrypt.hash(password, 12);
     const user = await User.create({
